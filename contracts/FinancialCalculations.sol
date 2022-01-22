c6b6021385c79cc2b6bd574fdcc32fab7cd20934
Hugo Forte <hugoforte@users.noreply.github.com>
2022-01-22 09:00:19 -0600

    Rudamentary test passing in a variable

---
+++ contracts/FinancialCalculations.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+
+contract FinancialCalculations {
+
+    constructor() {
+    }
+
+    function irr(uint guess) public pure returns(uint){
+        uint retVal = guess;
+        return retVal;
+    }
+}
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,11 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+describe("FinancialCalculations", function () {
+  it("Should return guess", async function () {
+    const FinancialCalculations = await ethers.getContractFactory("FinancialCalculations");
+    const financialCalculations = await FinancialCalculations.deploy();
+    await financialCalculations.deployed();
+    expect(await financialCalculations.irr(10)).to.equal(10);
   });
 });
